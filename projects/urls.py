6fa26e998df9958c6c4feb7ca3b088c572324717
Jacob Tippit <jake.p.tippit@gmail.com>
2022-10-24 16:45:09 -0500

    Feature 05 complete

---
+++ projects/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from projects.views import (
+    project_list,
+)
+
+urlpatterns = [
+    path("", project_list, name="list_projects"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from projects.models import Project
+
+
+def project_list(request):
+    projects = Project.objects.all()
+    context = {
+        "project_list": projects,
+    }
+    return render(request, "projects/list.html", context)
+
 
 # Create your views here.
