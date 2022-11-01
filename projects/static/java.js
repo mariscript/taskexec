4e6da9a0dd49b67d4d6a68cd5a6c85432d92da25
Jacob Tippit <jake.p.tippit@gmail.com>
2022-11-01 16:34:59 -0500

    Dark mode be workin

--- projects/static/java.js
+++ projects/static/java.js
@@ -3,3 +3,12 @@ const htmlEl = document.getElementsByTagName('html')[0];
 const toggleTheme = (theme) => {
     htmlEl.dataset.theme = theme;
 }
+
+function tgl() {
+    var darkswitch = document.getElementById("darkswitch");
+    if (darkswitch.checked == true) {
+        htmlEl.dataset.theme = 'dark';
+    } else {
+        htmlEl.dataset.theme = 'light';
+    }
+}
