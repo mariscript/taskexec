fc4015a472d890eb366c81295625b739a3b27428
Jacob Tippit <jake.p.tippit@gmail.com>
2022-11-02 16:31:43 -0500

    Dark mode is done

--- projects/static/java.js
+++ projects/static/java.js
@@ -1,17 +1,29 @@
 const htmlEl = document.getElementsByTagName('html')[0];
-
+const darkswitch = document.getElementById("darkswitch");
+console.log(darkswitch)
 const toggleTheme = (theme) => {
     htmlEl.dataset.theme = theme;
 }
 
 function tgl() {
-    var darkswitch = document.getElementById("darkswitch");
     if (darkswitch.checked == true) {
-        htmlEl.dataset.theme = 'dark';
+        localStorage.setItem('darkswitch', true);
+        if (localStorage.getItem('darkswitch') == 'true')
+            htmlEl.dataset.theme = 'dark';
     } else {
         htmlEl.dataset.theme = 'light';
+        localStorage.setItem('darkswitch', false);
     }
 }
 
+if (localStorage.getItem('darkswitch') == 'true') {
+    htmlEl.dataset.theme = 'dark';
+    darkswitch.checked = true;
 
+
+}
+else {
+    htmlEl.dataset.theme = 'light';
+    darkswitch.checked = false;
+}
 
