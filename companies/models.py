79613dd10c08375d92ec942e1b8ff7da3434cb29
Marison <your@gmail.here>
2022-11-03 13:07:14 -0400

    chart view button

---
+++ companies/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+
+# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="companies",
+        on_delete=models.CASCADE,
+    )
+
+    def str(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Companies"
